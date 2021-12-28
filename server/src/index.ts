b4c7455b70abdf330455a91b40d703a6f5d6030b
esow <simmerkaer@gmail.com>
2021-12-28 23:56:32 +0100

    Added eslint, prettier and type-graphql

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,51 +1,43 @@
-import "reflect-metadata";
-import express from "express";
 import { ApolloServer } from "apollo-server-express";
+import express from "express";
+import "reflect-metadata";
+import { buildSchema } from "type-graphql";
+import { UserResolver } from "./resolvers";
 
 (async () => {
+  // tslint:disable-next-line: typedef
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [UserResolver],
+    }),
+  });
 
-    // tslint:disable-next-line: typedef
-    const apolloServer = new ApolloServer({
-        typeDefs: `
-           type Query {
-               hello: String
-           }
-        `,
-        resolvers: {
-            Query: {
-                hello: () => "hello world"
-            }
-        }
+  // tslint:disable-next-line: typedef
+  const app = express();
+  apolloServer.start().then((_) => {
+    apolloServer.applyMiddleware({ app });
+
+    app.get("/", (_req, res) => res.send("hello"));
+    app.listen(4000, () => {
+      console.log("express server started");
     });
-
-    // tslint:disable-next-line: typedef
-    const app = express();
-    apolloServer.start().then(_ => {
-        apolloServer.applyMiddleware({app});
-
-        app.get("/", (_req, res) => res.send("hello"));
-        app.listen(4000, () => {
-            console.log("express server started");
-        });
-    });
-
-
+  });
 })();
 
 // createConnection().then(async connection => {
 
-    // console.log("Inserting a new user into the database...");
-    // const user = new User();
-    // user.firstName = "Timber";
-    // user.lastName = "Saw";
-    // user.age = 25;
-    // await connection.manager.save(user);
-    // console.log("Saved a new user with id: " + user.id);
+// console.log("Inserting a new user into the database...");
+// const user = new User();
+// user.firstName = "Timber";
+// user.lastName = "Saw";
+// user.age = 25;
+// await connection.manager.save(user);
+// console.log("Saved a new user with id: " + user.id);
 
-    // console.log("Loading users from the database...");
-    // const users = await connection.manager.find(User);
-    // console.log("Loaded users: ", users);
+// console.log("Loading users from the database...");
+// const users = await connection.manager.find(User);
+// console.log("Loaded users: ", users);
 
-    // console.log("Here you can setup and run express/koa/any other framework.");
+// console.log("Here you can setup and run express/koa/any other framework.");
 
 // }).catch(error => console.log(error));
