aa44fba70ddef6ba1bed28107c08b4a806ac1671
esow <simmerkaer@gmail.com>
2022-01-09 22:25:31 +0100

    Add login and register resolver

--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -1,6 +1,20 @@
-import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import {
+  Arg,
+  Field,
+  Mutation,
+  ObjectType,
+  Query,
+  Resolver,
+} from "type-graphql";
 import { User } from "./entity/User";
+import { compare, hash } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 
+@ObjectType()
+class LoginResponse {
+  @Field()
+  accessToken: string;
+}
 @Resolver()
 export class UserResolver {
   @Query(() => String)
@@ -13,15 +27,40 @@ export class UserResolver {
     return User.find();
   }
 
+  @Mutation(() => LoginResponse)
+  async login(
+    @Arg("email") email: string,
+    @Arg("password") password: string
+  ): Promise<LoginResponse> {
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      throw new Error("Could not find user");
+    }
+
+    const valid = await compare(password, user.password);
+
+    if (!valid) {
+      throw new Error("Invalid password");
+    }
+
+    // Login success
+    return {
+      accessToken: sign({ userId: user.id }, "secret", { expiresIn: "15m" }),
+    };
+  }
+
   @Mutation(() => Boolean)
   async register(
     @Arg("email") email: string,
     @Arg("password") password: string
   ) {
+    console.log("hashing");
+    const hashedPassword = await hash(password, 1);
+    console.log("hashed", hashedPassword);
     try {
       await User.insert({
         email,
-        password,
+        password: hashedPassword,
       });
     } catch (error) {
       console.log(error);
